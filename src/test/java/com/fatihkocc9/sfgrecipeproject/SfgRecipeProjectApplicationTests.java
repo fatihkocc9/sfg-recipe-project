d32f7a208efa8636cc7fa15a5bde474863d84188
Fatih Koç <fatihkoc@hotmail.it>
2021-08-31 20:55:59 +0300

    Maven files

---
+++ src/test/java/com/fatihkocc9/sfgrecipeproject/SfgRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatihkocc9.sfgrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
